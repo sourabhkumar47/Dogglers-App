d307ce9be5a495f128d4d68c26d798e0c003420c
sourabhkumar47 <20010001030@gateway.edu.in>
2022-04-09 12:52:53 +0530

    Added Code of Program

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/example/dogglers/databinding/GridListItemBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/example/dogglers/databinding/GridListItemBinding.java
@@ -4,20 +4,41 @@ package com.example.dogglers.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.example.dogglers.R;
 import com.google.android.material.card.MaterialCardView;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class GridListItemBinding implements ViewBinding {
   @NonNull
   private final MaterialCardView rootView;
 
-  private GridListItemBinding(@NonNull MaterialCardView rootView) {
+  @NonNull
+  public final ImageView dogimg;
+
+  @NonNull
+  public final TextView textAge;
+
+  @NonNull
+  public final TextView textHobbies;
+
+  @NonNull
+  public final TextView textName;
+
+  private GridListItemBinding(@NonNull MaterialCardView rootView, @NonNull ImageView dogimg,
+      @NonNull TextView textAge, @NonNull TextView textHobbies, @NonNull TextView textName) {
     this.rootView = rootView;
+    this.dogimg = dogimg;
+    this.textAge = textAge;
+    this.textHobbies = textHobbies;
+    this.textName = textName;
   }
 
   @Override
@@ -43,10 +64,38 @@ public final class GridListItemBinding implements ViewBinding {
 
   @NonNull
   public static GridListItemBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.dogimg;
+      ImageView dogimg = ViewBindings.findChildViewById(rootView, id);
+      if (dogimg == null) {
+        break missingId;
+      }
 
-    return new GridListItemBinding((MaterialCardView) rootView);
+      id = R.id.textAge;
+      TextView textAge = ViewBindings.findChildViewById(rootView, id);
+      if (textAge == null) {
+        break missingId;
+      }
+
+      id = R.id.textHobbies;
+      TextView textHobbies = ViewBindings.findChildViewById(rootView, id);
+      if (textHobbies == null) {
+        break missingId;
+      }
+
+      id = R.id.textName;
+      TextView textName = ViewBindings.findChildViewById(rootView, id);
+      if (textName == null) {
+        break missingId;
+      }
+
+      return new GridListItemBinding((MaterialCardView) rootView, dogimg, textAge, textHobbies,
+          textName);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
--- app/src/main/java/com/example/dogglers/adapter/DogCardAdapter.kt
+++ app/src/main/java/com/example/dogglers/adapter/DogCardAdapter.kt
@@ -16,11 +16,15 @@
 package com.example.dogglers.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dogglers.R
-import javax.sql.DataSource
+import com.example.dogglers.const.Layout
+import com.example.dogglers.data.DataSource
 
 /**
  * Adapter to inflate the appropriate list item layout and populate the view with information
@@ -32,7 +36,7 @@ class DogCardAdapter(
 ): RecyclerView.Adapter<DogCardAdapter.DogCardViewHolder>() {
 
     // TODO: Initialize the data using the List found in data/DataSource
-    val myDataSet = com.example.dogglers.data.DataSource.dogs
+    val dataset = DataSource.dogs
 
 //    val myDataset = DataSource().loadAffirmations()
 //    val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
@@ -43,6 +47,11 @@ class DogCardAdapter(
      */
     class DogCardViewHolder(view: View?): RecyclerView.ViewHolder(view!!) {
         // TODO: Declare and initialize all of the list item UI components
+
+        val imageView:ImageView= view!!.findViewById( R.id.dogimg)
+        val textName:TextView=view!!.findViewById(R.id.textName)
+        val textAge:TextView=view!!.findViewById(R.id.textAge)
+        val textHobbies:TextView=view!!.findViewById(R.id.textHobbies)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogCardViewHolder {
@@ -54,20 +63,44 @@ class DogCardAdapter(
 
         // TODO: Null should not be passed into the view holder. This should be updated to reflect
         //  the inflated layout.
-        return DogCardViewHolder(null)
+
+        return when(layout){
+            Layout.GRID -> {
+                val adapterLayout = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.grid_list_item,parent,false)
+                DogCardViewHolder(adapterLayout)
+            }
+            else -> {
+                    val adapterLayout = LayoutInflater.from(parent.context)
+                        .inflate(R.layout.vertical_horizontal_list_item,parent,false)
+                    DogCardViewHolder(adapterLayout)
+
+            }
+        }
     }
 
-    override fun getItemCount(): Int = 0 // TODO: return the size of the data set instead of 0
+    override fun getItemCount(): Int = dataset.size // TODO: return the size of the data set instead of 0
 
     override fun onBindViewHolder(holder: DogCardViewHolder, position: Int) {
         // TODO: Get the data at the current position
+        val item = dataset[position]
+
         // TODO: Set the image resource for the current dog
+        holder.imageView.setImageResource(item.imageResourceId)
+
         // TODO: Set the text for the current dog's name
+        holder.textName.text = item.name
+
         // TODO: Set the text for the current dog's age
+
         val resources = context?.resources
         // TODO: Set the text for the current dog's hobbies by passing the hobbies to the
         //  R.string.dog_hobbies string constant.
         //  Passing an argument to the string resource looks like:
         //  resources?.getString(R.string.dog_hobbies, dog.hobbies)
+
+        holder.textAge.text = resources?.getString(R.string.dog_age ,item.age )
+
+        holder.textHobbies.text = resources?.getString(R.string.dog_hobbies ,item.hobbies )
     }
 }
